888df4cd0e2d1749f2316edcfa88d34840f8d666
SheFing <shevchukm2002@gmail.com>
2025-10-06 14:20:24 +0300

    attemp 1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-// write code here
+const placeholders = {
+  'sign-up-name': 'First name',
+  'sign-up-last-name': 'Last name',
+  'sign-up-email': 'Email',
+  'sign-up-password': 'Password',
+  'sign-in-email': 'Email',
+  'sign-in-password': 'Password',
+};
+
+Object.keys(placeholders).forEach((id) => {
+  const input = document.getElementById(id);
+
+  if (input) {
+    input.setAttribute('placeholder', placeholders[id]);
+
+    const field = input.parentElement;
+    const newLabel = document.createElement('label');
+
+    newLabel.setAttribute('for', placeholders[id]);
+    newLabel.className = 'field-label';
+    newLabel.textContent = placeholders[id];
+    field.prepend(newLabel);
+  }
+});
