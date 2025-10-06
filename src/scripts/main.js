c1aed5ff62c697baba5076bfb887c65313080ab9
SheFing <shevchukm2002@gmail.com>
2025-10-06 16:09:43 +0300

    att3

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,36 @@
 'use strict';
 
-document.querySelectorAll('.field-text').forEach((id) => {
-  const idName = id.id;
-  const textTemp = idName.split('-').slice(2).join(' ');
-  const textLabel = textTemp.charAt(0).toUpperCase() + textTemp.slice(1);
-  const input = document.getElementById(idName);
-
-  if (input) {
-    input.setAttribute('placeholder', textLabel);
-
-    const field = input.parentElement;
-    const newLabel = document.createElement('label');
-
-    newLabel.setAttribute('for', idName);
-    newLabel.className = 'field-label';
-    newLabel.textContent = textLabel;
-    field.prepend(newLabel);
+function createTextForLabel(text) {
+  if (text.includes('Name')) {
+    return text
+      .replace(/([A-Z])/, ' $1') // вставляємо один пробіл
+      .replace(/^./, (c) => c.toUpperCase()) // перша літера велика
+      .replace(/ ([A-Z])/, (_, c) => ' ' + c.toLowerCase()) // друга — мала
+      .trim();
   }
+
+  return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+const forms = document.querySelectorAll('form');
+
+forms.forEach((form) => {
+  const inputs = form ? form.querySelectorAll('input') : [];
+
+  inputs.forEach((id) => {
+    const textLabel = createTextForLabel(id.name);
+    const input = document.getElementById(id.id);
+
+    if (input) {
+      input.setAttribute('placeholder', textLabel);
+
+      const field = input.parentElement;
+      const newLabel = document.createElement('label');
+
+      newLabel.setAttribute('for', id.id);
+      newLabel.className = 'field-label';
+      newLabel.textContent = textLabel;
+      field.insertBefore(newLabel, input);
+    }
+  });
 });
